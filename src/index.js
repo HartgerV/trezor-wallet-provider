651f038194c5d34f409b369b0574407b38e3e9a1
rodrigopavezi <rodrigopavezi@gmail.com>
2019-01-30 14:43:34 -0200

    fix: instantiation

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import TrezorWallet from './TrezorWallet';
 
 export default async function (
   networkId,
-  pathOverride,
+  accountsOffset,
+  accountsQuantity,
+  eventEmitter
 ) {
   if(networkId == null) {
     networkId = 1; //default to mainnet network id
   }
   const trezor = new TrezorWallet(
     networkId,
-    pathOverride,
+    accountsOffset,
+    accountsQuantity,
+    eventEmitter,
   );
 
   return new HookedWalletSubprovider(trezor);
